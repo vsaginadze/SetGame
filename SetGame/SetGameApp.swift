6ab84b75eda2d38e3e6b5166f3c24785e4ae9c9b
Vakhtang Saginadze <vakhtang_saginadze@protonmail.com>
2024-03-04 17:44:34 +0400

    Initial Commit

---
+++ SetGame/SetGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetGameApp.swift
+//  SetGame
+//
+//  Created by Vakhtang Saginadze on 04.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct SetGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
