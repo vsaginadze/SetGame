62f7ab6facd9dea8576c9c3f1db3979ab9c4ab6a
Vakhtang Saginadze <vakhtang_saginadze@protonmail.com>
2024-03-07 22:43:18 +0400

    added AspectVGrid

---
+++ SetGame/AspectVGrid.swift
@@ -0,0 +1,64 @@
+//
+//  AspectVGrid.swift
+//  SetGame
+//
+//  Created by Vakhtang Saginadze on 07.03.2024.
+//
+
+//
+//  AspectViewGrid.swift
+//  MemorizeApp
+//
+//  Created by Vakhtang Saginadze on 13.02.2024.
+//
+
+import SwiftUI
+
+struct AspectViewGrid<Item: Identifiable, ItemView: View>: View {
+    var items: [Item]
+    var aspectRatio: CGFloat = 1
+    var content: (Item) -> ItemView
+    
+    init(_ items: [Item], aspectRatio: CGFloat, @ViewBuilder content: @escaping (Item) -> ItemView) {
+        self.items = items
+        self.aspectRatio = aspectRatio
+        self.content = content
+    }
+    
+    var body: some View {
+        GeometryReader { geometry in
+            let widthThatFits = gridItemWidthThatFits(
+                count: items.count,
+                size: geometry.size,
+                atAspectRatio: aspectRatio)
+            
+            LazyVGrid(columns: [GridItem(.adaptive(minimum: widthThatFits), spacing: 0)], spacing: 0) {
+                ForEach(items) { item in
+                    content(item)
+                        .aspectRatio(aspectRatio, contentMode: .fit)
+                }
+            }
+        }
+    }
+    
+    private func gridItemWidthThatFits(
+        count: Int,
+        size: CGSize,
+        atAspectRatio aspectRatio: CGFloat
+    ) -> CGFloat {
+        let count = CGFloat(count)
+        var columnCount = 1.0
+        repeat {
+            let width = size.width / columnCount
+            let height = width / aspectRatio
+            
+            let rowCount = (count / columnCount).rounded(.up)
+            if rowCount * height < size.height {
+                return (size.width / columnCount).rounded(.down)
+            }
+            columnCount += 1
+        } while columnCount < count
+        return min(size.width / count, size.height * aspectRatio).rounded(.down)
+    }
+    
+}
--- SetGame/ContentView.swift
+++ SetGame/ContentView.swift
@@ -10,15 +10,24 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            ZStack {
+                let base = RoundedRectangle(cornerRadius: 12)
+                
+                Group {
+                    base.fill(.white)
+                    base.stroke(lineWidth: 2)
+                    
+                    Text("shape")
+                        .font(.system(size: 200))
+                        .minimumScaleFactor(0.01)
+                        .aspectRatio(contentMode: .fit)
+                                            
+                }
+            }
         }
         .padding()
     }
 }
-
 #Preview {
     ContentView()
 }
