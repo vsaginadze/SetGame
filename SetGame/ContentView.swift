826f0330c6d0bdd0d0433f35a8b438964f8b864c
Vakhtang Saginadze <vakhtang_saginadze@protonmail.com>
2024-03-07 23:43:17 +0400

    added Diamond Shape

--- SetGame/ContentView.swift
+++ SetGame/ContentView.swift
@@ -13,19 +13,55 @@ struct ContentView: View {
             ZStack {
                 let base = RoundedRectangle(cornerRadius: 12)
                 
+                let oval = RoundedRectangle(cornerRadius: 50)
+                    .frame(width: 100, height: 300)
+                    .padding()
+                
+                let rectangle = Rectangle()
+                    .frame(width: 100, height: 150)
+                                    .padding()
+                                    
                 Group {
                     base.fill(.white)
                     base.stroke(lineWidth: 2)
                     
-                    Text("shape")
-                        .font(.system(size: 200))
-                        .minimumScaleFactor(0.01)
-                        .aspectRatio(contentMode: .fit)
-                                            
+                    VStack {
+                        ForEach(0..<3, id: \.self) { idx in
+                            Diamond()
+                                .frame(width: 200, height: 150)
+                                .padding()
+                        }
+                    }
                 }
             }
         }
         .padding()
+    }
+    
+    struct Diamond: Shape {
+        func path(in rect: CGRect) -> Path {
+            let width = rect.width
+            let height = rect.height
+            
+            let centerX = rect.midX
+            let centerY = rect.midY
+            
+            var path = Path()
+            
+            // Move to the top point
+            path.move(to: CGPoint(x: centerX, y: 0))
+            
+            // Draw the top-right line
+            path.addLine(to: CGPoint(x: width, y: centerY))
+            
+            path.addLine(to: CGPoint(x: centerX, y: height))
+            
+            path.addLine(to: CGPoint(x: 0, y: centerY))
+            
+            path.closeSubpath()
+            
+            return path
+        }
     }
 }
 #Preview {
