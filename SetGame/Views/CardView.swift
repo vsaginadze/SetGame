74d50d1b50deeb5fa6ef818745407be2d0dba002
Vakhtang Saginadze <vakhtang_saginadze@protonmail.com>
2024-03-13 05:16:40 +0400

    card view for set game

--- SetGame/Views/CardView.swift
+++ SetGame/Views/CardView.swift
@@ -8,7 +8,9 @@
 import SwiftUI
 
 struct CardView: View {
-    let card: SetGameModel.Card
+    typealias Card = SetGameModel.Card
+    
+    let card: Card
     
     var body: some View {
         ZStack {
@@ -19,33 +21,74 @@ struct CardView: View {
                 base.stroke(lineWidth: 2)
                 
                 cardContent
+                    .foregroundStyle(color)
             }
-        }
-    }
-    
-    @ViewBuilder
-    private var shape: some View {
-        if card.shape == "diamond" {
-            Diamond()
-        } else if card.shape == "oval" {
-            RoundedRectangle(cornerRadius: 12)
-        } else if card.shape == "rectangle" {
-            Rectangle()
         }
     }
     
     private var cardContent: some View {
         VStack {
-            ForEach(0..<card.numberOfItems, id: \.self) { _ in
+            ForEach(0..<card.features.number, id: \.self) { _ in
                 shape
-                    .aspectRatio(2/1, contentMode: .fill)
+                    .aspectRatio(2/1, contentMode: .fit)
                     .minimumScaleFactor(0.01)
+                    .opacity(getOpacity(card.features.shading))
             }
         }
         .padding()
     }
+    
+    
+    @ViewBuilder
+    private var shape: some View {
+        if card.features.shape == "diamond" {
+            if card.features.shading == "open" {
+                Diamond()
+                    .stroke(lineWidth: 2)
+            } else {
+                Diamond()
+            }
+        } else if card.features.shape == "oval" {
+            if card.features.shading == "open" {
+                Capsule()
+                    .stroke(lineWidth: 2)
+            } else {
+                Capsule()
+            }
+        } else if card.features.shape == "rectangle" {
+            if card.features.shading == "open" {
+                Rectangle()
+                    .stroke(lineWidth: 2)
+            } else {
+                Rectangle()
+            }
+        }
+    }
+    
+    
+    private var color: Color {
+        if card.features.color == "orange" {
+            return Color.orange
+        } else if card.features.color == "red" {
+            return Color.red
+        } else if card.features.color == "purple" {
+            return Color.purple
+        }
+        
+        return Color.orange
+    }
+    
+    private func getOpacity(_ shading: String) -> Double {
+        if shading == "solid" || shading == "open" {
+            return 1
+        } else if shading == "striped" {
+            return 0.5
+        }
+        
+        return 1
+    }
 }
 
 #Preview {
-    CardView(card: SetGameModel.Card(id: 1, shape: "oval"))
+    CardView(card: SetGameModel.Card(id: 1, features: SetGameModel.Features("oval", "solid", 1, "orange")))
 }
