e3071b23374ab30fc17cd95542bf6af61744178e
Vakhtang Saginadze <vakhtang_saginadze@protonmail.com>
2024-03-13 05:15:32 +0400

    set game viewModel

--- SetGame/SetGameViewModel.swift
+++ SetGame/SetGameViewModel.swift
@@ -9,21 +9,34 @@ import Foundation
 
 class SetGameViewModel: ObservableObject {
     typealias Card = SetGameModel.Card
-    
-    private static let numberOfDisplayedCards: Int = 81
+    typealias CardFeatures = SetGameModel.Features
     
     init() {
         model = SetGameViewModel.createSetGame()
     }
     
     private static func createSetGame() -> SetGameModel {
-        SetGameModel(numberOfDisplayedCards: numberOfDisplayedCards)
+        SetGameModel(numberOfDisplayedCards: numberOfDisplayedCards) {
+            let shape = shapes.randomElement() ?? "oval"
+            let shading = shading.randomElement() ?? "solid"
+            let color = colors.randomElement() ?? "orange"
+            let number = Int.random(in: 1...3)
+            
+            return CardFeatures(shape, shading, number, color)
+        }
     }
     
     @Published var model: SetGameModel
+    
     var cards: Array<Card> {
         model.cards
     }
+    
+    // MARK: Optimizible
+    private static let numberOfDisplayedCards: Int = 4
+    private static let shapes = ["oval", "diamond", "rectangle"]
+    private static let shading = ["solid", "striped", "open"]
+    private static let colors = ["orange", "red", "purple"]
     
     // MARK: Intents
     
