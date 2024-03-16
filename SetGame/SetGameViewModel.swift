15a0f874d633d862302f6b9808983e0ccdfd5bf0
Vakhtang Saginadze <vakhtang_saginadze@protonmail.com>
2024-03-16 21:14:28 +0400

    moved from string based features to enum based features

--- SetGame/SetGameViewModel.swift
+++ SetGame/SetGameViewModel.swift
@@ -9,42 +9,16 @@ import Foundation
 
 class SetGameViewModel: ObservableObject {
     typealias Card = SetGameModel.Card
-    typealias CardFeatures = SetGameModel.Features
-    
-    init() {
-        model = SetGameViewModel.createSetGame()
-    }
-    
-    private static func createSetGame() -> SetGameModel {
-        SetGameModel(featuresFactory)
-    }
-    
-    private static func featuresFactory() -> CardFeatures {
-        let shape = shapes.randomElement() ?? "oval"
-        let shading = shading.randomElement() ?? "solid"
-        let color = colors.randomElement() ?? "orange"
-        let number = Int.random(in: 1...3)
-        
-        return CardFeatures(shape, shading, number, color)
-    }
-    
-    
-    @Published var model: SetGameModel
-    
+
+    @Published var model: SetGameModel = SetGameModel()
+
     var cards: Array<Card> {
-        model.cards
+        model.dealedCards
     }
-    
-    
-    // MARK: Optimizible
-    private static var numberOfDisplayedCards = 12
-    private static let shapes = ["oval", "diamond", "rectangle"]
-    private static let shading = ["solid", "striped", "open"]
-    private static let colors = ["orange", "red", "purple"]
     
     // MARK: Intents
     func addThreeCards() {
-        
+        model.deal()
     }
     
     func startNewGame() {
