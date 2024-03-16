fb4571b9073a1cf4f870af108ba1bd3d8a0f04ca
Vakhtang Saginadze <vakhtang_saginadze@protonmail.com>
2024-03-16 21:14:15 +0400

    moved from string based features to enum based features

--- SetGame/SetGameModel.swift
+++ SetGame/SetGameModel.swift
@@ -8,49 +8,95 @@
 import Foundation
 
 struct SetGameModel {
-    private(set) var cards: Array<Card>
+    private(set) var dealedCards: Array<Card>
+    private(set) var deck: Deck
     
-    init(_ featuresFactory: () -> Features) {
-        self.cards = []
+    init() {
+        dealedCards = []
+        deck = Deck()
         
-        for i in 0..<25 {
-            let card = Card(id: i, features: featuresFactory())
-            cards.append(card)
-        }
+        deal(12)
     }
     
     mutating func shuffle() {
-        cards.shuffle()
+        dealedCards.shuffle()
+    }
+
+    mutating func deal(_ n: Int = 3) {
+        for _ in 0..<n {
+            guard let card = deck.drawCard() else {
+                print("no more cards in deck")
+                return
+            }
+            dealedCards.append(card)
+        }
     }
     
+    struct Deck {
+        private var cards = [Card]()
+        
+        init() {
+            for number in Number.allCases {
+                for color in Color.allCases {
+                    for shape in Shape.allCases {
+                        for shading in Shading.allCases {
+                            cards.append(Card(shape: shape, shading: shading, number: number, color: color))
+                        }
+                    }
+                }
+            }
+            cards.shuffle()
+        }
+        
+        mutating func drawCard() -> Card? {
+            guard cards.count > 0 else {
+                return nil
+            }
+            return cards.removeFirst()
+        }
+    }
     
     struct Card: Identifiable {
-        let features: Features
-        var isSelected: Bool = false
-        var id: Int
+        // features
+        let shape: Shape
+        let shading: Shading
+        let number: Number
+        let color : Color
+        
         var state: CardState = .fresh
         
-        init(id: Int, features: Features) {
-            self.id = id
-            self.features = features
-        }
+        var isSelected: Bool = false
+        var id = UUID()
     }
     
     enum CardState {
         case fresh, playing, matched
     }
     
-    struct Features {
-        let shape: String
-        let shading: String
-        let number: Int
-        let color : String
-        
-        init(_ shape: String, _ shading: String, _ number: Int, _ color: String) {
-            self.shape = shape
-            self.shading = shading
-            self.number = number
-            self.color = color
+    enum Number: Int, CaseIterable, Hashable {
+        case one = 1, two, three
+    }
+
+    enum Color: String, CaseIterable, Hashable {
+        case red = "🟥", purple = "🟪", green = "🟩"
+    }
+
+    enum Shading: String, CaseIterable, Hashable {
+        case solid, stripped, outlined
+    }
+
+    enum Shape: String, CaseIterable, Hashable, CustomStringConvertible {
+        case oval , squiggle, diamond
+
+        var description: String {
+            switch self {
+                case .oval:
+                    return "O"
+                case .squiggle:
+                    return "~"
+                case .diamond:
+                    return "◇"
+            }
         }
     }
 }
