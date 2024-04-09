c233fb26e689c4d79b84a97d10f466527fcd7ea3
Pastel <99262977+AtifYagiz@users.noreply.github.com>
2024-04-09 15:07:47 +0300

    Fixed bugs
    Fixed cards disappearing without showing green.

--- SetGame/SetGameViewModel.swift
+++ SetGame/SetGameViewModel.swift
@@ -6,10 +6,26 @@
 //
 
 import Foundation
+import SwiftUI
 
 class SetGameViewModel: ObservableObject {
     typealias Card = SetGameModel.Card
-    @Published var model: SetGameModel = SetGameModel()
+    @Published var model: SetGameModel = SetGameModel() {
+        didSet {
+            if !model.willDealCards.isEmpty {
+                let cards = model.willDealCards.removeLast()
+                DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(1)) {
+                    withAnimation {
+                        self.model.dealedCards.removeAll { card in
+                            return cards.0.id == card.id || cards.1.id == card.id || cards.2.id == card.id
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
     
     var allCardsArePresent = false
     var noMoreCards: Bool {
